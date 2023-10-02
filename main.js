065f234715e67f88cb030e3c899b982de385b40f
xXHoGodXx <142801872+xXHoGodXx@users.noreply.github.com>
2023-10-02 15:22:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+let students = [
+    {
+        name: "baxtzod",
+        age: 15,
+        isMarried: false,
+        hair_color: "blue"
+    },
+    {
+        name: "islom",
+        age: 15,
+        isMarried: true,
+        hair_color: "red"
+    },
+    {
+        name: "safir",
+        age: 15,
+        isMarried: false,
+        hair_color: "lisiy"
+    },
+    {
+        name: "abdumanon",
+        age: 13,
+        isMarried: false,
+        hair_color: "black"
+    },
+    {
+        name: "xojimurod",
+        age: 16,
+        isMarried: true,
+        hair_color: "pink"
+    },
+    {
+        name: "aziza soliyeva",
+        age: 15,
+        isMarried: true,
+        hair_color: "green"
+    },
+    {
+        name: "aziza",
+        age: 15,
+        isMarried: false,
+        hair_color: "skyblue"
+    },
+    {
+        name: "ismoil",
+        age: 12,
+        isMarried: true,
+        hair_color: "brown"
+    },
+    {
+        name: "artyom",
+        age: 14,
+        isMarried: false,
+        hair_color: "purple"
+    },
+    {
+        name: "abdukhafiz",
+        age: 17,
+        isMarried: false,
+        hair_color: "white"
+    },
+    {
+        name: "bobur",
+        age: 18,
+        isMarried: true,
+        hair_color: "brown"
+    },
+    {
+        name: "shoxa",
+        age: 17,
+        isMarried: false,
+        hair_color: "black"
+    },
+    {
+        name: "alSafir",
+        age: 15,
+        isMarried: true,
+        hair_color: "orange"
+    },
+]
+
+let age = Object(students).age.sort((a, b) => a - b)
+
+console.log(age);
+
+students.filter(students=> {
+    if(students.isMarried===true && students.isMarried===false ){
+        Object.values(students.name)
+    }
+})
+
+students.filter(Max.math(students))
+students.filter(Min.math(students))
